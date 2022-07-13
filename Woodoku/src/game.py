cae644ebd13a7e3873d5221f18ca6323579f0408
Haoran Li <edward.haoran.lee@outlook.com>
2022-07-12 23:05:27 -0700

    Implemented game logic

---
+++ Woodoku/src/game.py
@@ -0,0 +1,80 @@
+from typing import List
+
+from Woodoku.src.Entity.WoodokuBoard import WoodokuBoard
+from Woodoku.src.Entity.WoodokuShape import WoodokuShape
+from Woodoku.src.UI.CommandLineUI import CommandLineUI
+from Woodoku.src.UI.InterfaceUI import InterfaceUI
+
+CONFIG_FILE = ''
+NUM_SHAPES = 3
+
+
+def read_shapes_from_file(filepath: str) -> List[WoodokuShape]:
+    pass
+
+
+def random_shapes(shapes: List[WoodokuShape], num: int) -> List[WoodokuShape]:
+    """
+    Choose <num> non-repeated random shapes from <shapes>.
+
+    :param shapes: All shapes to choose.
+    :param num: Number of shapes to choose.
+    :return: The shapes for this round.
+    """
+    pass
+
+
+def check_if_lose(board: WoodokuBoard, shapes: List[WoodokuShape], shape_availability: List[bool]) -> bool:
+    pass
+
+
+def game(ui: InterfaceUI) -> None:
+    board = WoodokuBoard()
+    board.initialize()
+
+    # Generating all possible shapes that might appear in game.
+    all_shapes = read_shapes_from_file(CONFIG_FILE)
+
+    ui.show_start_game(board)
+
+    score = 0
+
+    while True:
+
+        # Select NUM_SHAPES shapes from all possible shapes.
+        shapes = random_shapes(all_shapes, NUM_SHAPES)
+
+        # All selected shapes are available to be chosen.
+        shape_availability = [True] * NUM_SHAPES
+
+        # When there is any shape in this round still waiting to be chosen.
+        while any(shape_availability):
+
+            if check_if_lose(board, shapes, shape_availability):
+                ui.show_result(board, shapes, shape_availability)
+                return
+
+            ui.show_board(board)
+
+            # Let user choose shape.
+            shape_index = ui.choose_shape(shapes, shape_availability)
+
+            # Let user place at location.
+            x, y = ui.put_shape_at()
+
+            # Check if the chosen shape can be placed at location.
+            if board.can_add_shape_at_location(shapes[shape_index], x, y):
+                shape_availability[shape_index] = False
+
+                board.add_shape(shapes[shape_index], x, y)
+
+                new_score = board.get_score()
+                ui.show_earned(new_score - score)
+                score = new_score
+
+            else:
+                ui.show_cannot_place()
+
+
+if __name__ == '__main__':
+    game(CommandLineUI())
