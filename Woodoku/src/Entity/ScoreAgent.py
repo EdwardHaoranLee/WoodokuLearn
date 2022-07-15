103e7db7be77dba994976bc87835ad2101924b2b
Richard Huang <Xiaorui-Richard.Huang@ibm.com>
2022-07-15 06:29:38 -0400

    refactor(Score): doctring terminology renaming

--- Woodoku/src/Entity/ScoreAgent.py
+++ Woodoku/src/Entity/ScoreAgent.py
@@ -7,11 +7,12 @@ class ScoreAgent:
     def __init__(self):
         pass
 
-    def calculate_winning(self, winning_map: Dict[str, int]) -> None:
-        """Given `winning_map`, calculate score and add to score
+    def calculate_winning(self, group_info: Dict[str, int]) -> None:
+        """Given `groups`, calculate score and add to score
 
         Args:
-            winning_map (Dict[str, int]): _description_
+            group_info (Dict[str, int]): A dictionary mapping from name of the
+            group to the number of groups. e.g. { "row": 1, "column": 2, "box": 0 }
         """
         pass
 
