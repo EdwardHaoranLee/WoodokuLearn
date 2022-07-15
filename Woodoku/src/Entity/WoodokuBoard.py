0bd51e8310075a7c5820d143558775a9f87088ab
Richard Huang <Xiaorui-Richard.Huang@ibm.com>
2022-07-15 01:11:20 -0400

    docs(WoodokuBoard): google style docstring rewrite

--- Woodoku/src/Entity/WoodokuBoard.py
+++ Woodoku/src/Entity/WoodokuBoard.py
@@ -4,8 +4,7 @@ from typing import Dict, List, Tuple
 
 
 class WoodokuBoardRepresentation:
-    """
-    The actual low-level implementation of the board. Data class.
+    """A data class representing the low-level implementation of the board
 
     The top-left block's coordinate is (0, 0).
     The last block on the top row is (0, n - 1).
@@ -25,16 +24,24 @@ class WoodokuBoardRepresentation:
         pass
 
     def is_occupied(self, blocks_coord: List[Tuple[int, int]]) -> bool:
-        """
-        Check if each coord has block on it. If all of those coords are occupied, return true. Otherwise, false.
+        """Check if each coord has a block on it. If all of those coords are
+        occupied, return true. Otherwise, false.
 
-        :param blocks_coord:
-        :return:
+        Args:
+            blocks_coord (List[Tuple[int, int]]): list or coordinates to check 
+
+        Returns:
+            bool: if any coordinates in `blocks_coord` is occupied
         """
         pass
 
 
 class WoodokuBoard:
+    """A 9x9 Woodoku Board
+
+    Raises:
+        CanNotAddShapeError: still cannot add shape after checking the board
+    """
     __scoreAgent: ScoreAgent
     __representation: WoodokuBoardRepresentation
 
@@ -48,36 +55,44 @@ class WoodokuBoard:
         pass
 
     def can_add_shape_to_board(self, shape: WoodokuShape) -> bool:
-        """
-        Check if the woodoku shape can fit into the board. If all current shapes are not be able to add, the game fails.
-        :return: if can fit.
+        """Check if the woodoku shape can fit into the board. If all current
+        shapes are not be able to add, the game fails.
+
+        Args:
+            shape (WoodokuShape): a woodoku shape for validation 
+
+        Returns:
+            bool: if the `shape` fits the board
         """
         pass
 
+
     def can_add_shape_at_location(self, shape: WoodokuShape, x: int, y: int) -> bool:
-        """
-        Check if the woodoku shape can be placed into the board at location (x, y) coordinates as the left upper corner
-        of the shape.
-        :param shape: the shape
-        :param x: x coordinate
-        :param y: y coordinate
-        :return: if can be placed at location.
+        """Check if the woodoku `shape` can be placed into the board at location
+        `(x, y)` coordinates as the left upper corner of the shape.
+
+        Args:
+            shape (WoodokuShape): The shape needed to be checked
+            x (int): x coordinate 
+            y (int): y coordinate 
+
+        Returns:
+            bool: if `shape` can be added to `(x,y)`
         """
         pass
 
     def add_shape(self, shape: WoodokuShape, x: int, y: int) -> None:
-        """
-        Add the shape to woodoku at coordinate (x, y). This method should be called after checking the addibility. If
-        unable to add, raise exception.
+        """Add the shape to woodoku at coordinate (x, y). Only called if the shape
+        can be added. If unable to add, raise exception.
 
         This method includes actions:
         1. Clear winning row/column/3x3 block
         2. Call scoreAgent to calculate score
 
-        :param shape: The shape
-        :param x: x coordinate
-        :param y: y coordinate
-        :return: none
+        Args:
+            shape (WoodokuShape): The shape to be added
+            x (int): x coordinate
+            y (int): y coordinate
         """
         pass
 
@@ -85,10 +100,12 @@ class WoodokuBoard:
         pass
 
     def __find_winning_blocks(self) -> Dict[str, int]:
-        """
-        Check current board and see if there is any winning blocks such as complete rows, columns or 3x3 block.
-        :return: A dictionary mapping from name of the winning block to the number of winning block.
-            e.g. { "row": 1, "column": 2, "3x3 block": 0 }
+        """Check current board and see if there is any winning blocks such as
+        complete rows, columns or 3x3 block.
+
+        Returns:
+            A dictionary mapping from name of the winning block to the number of
+            winning block. e.g. { "row": 1, "column": 2, "3x3 block": 0 }
         """
         pass
 
@@ -99,15 +116,17 @@ class WoodokuBoard:
         pass
 
     def __get_3x3_all_coords(self, index: int) -> List[Tuple[int, int]]:
-        """
-        The index of 3x3 block is as following:
+        """The index of 3x3 block is as following:
         _____________
         | 0 | 1 | 2 |
         | 3 | 4 | 5 |
         | 6 | 7 | 8 |
         _____________
 
-        :param index: the index as above
-        :return: All the coords in this 3x3 block.
+        Args:
+            index (int): The index of 3x3 block as list above
+
+        Returns:
+            List[Tuple[int, int]]: All the coordinates in that 3x3 block
         """
         pass
