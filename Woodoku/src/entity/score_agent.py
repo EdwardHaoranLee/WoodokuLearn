7e9797fa22620e69c078f3489f9fd2c9afa44e7c
Richard Huang <Xiaorui-Richard.Huang@ibm.com>
2022-07-22 23:33:14 -0400

    feat(score): implements sore agent and changes some api

--- Woodoku/src/entity/score_agent.py
+++ Woodoku/src/entity/score_agent.py
@@ -1,20 +1,44 @@
-from typing import Dict
+GROUP_POINTS = 18
+STREAK_POINTS = 10
+COMBO_POINTS = 28
 
 
 class ScoreAgent:
     __score: int
+    __streaks: int
 
     def __init__(self):
-        pass
+        self.__score = 0
+        self.__streaks = 0
 
-    def calculate_winning(self, group_info: Dict[str, int]) -> None:
+    def calculate_winning(self, blocks: int, groups: int) -> None:
         """Given `groups`, calculate score and add to score
 
+        Precondition: blocks > 0, groups >= 0
+
+        Scoring Rules:
+            1. 1 point for each block placed
+            2. A single groups scores 18 points
+            3. Each streak adds 10 points on top of the previous streak
+            4. Each additional combo adds 28 points
+
         Args:
-            group_info (Dict[str, int]): A dictionary mapping from name of the
-            group to the number of groups. e.g. { "row": 1, "column": 2, "box": 0 }
+            blocks (int): The number of blocks placed
+            groups (int): The number of groups completed
         """
-        pass
+        self.__score += blocks
+
+        if groups:
+            self.__score += GROUP_POINTS
+            if self.__streaks:
+                self.__score += STREAK_POINTS * self.__streaks
+            self.__streaks += 1
+        else:
+            self.__streaks = 0
+
+        combos = groups - 1
+        if combos > 0:
+            self.__score += COMBO_POINTS * combos
 
     def get_score(self) -> int:
         return self.__score
