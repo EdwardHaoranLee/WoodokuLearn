8068cb11476cb4269fcde2b6629ca623e8d63bb4
yanlinghu <yanling.hu@mail.utoronto.ca>
2022-07-22 22:08:17 -0400

    add type hint

--- Woodoku/tests/entity/test_woodoku_board.py
+++ Woodoku/tests/entity/test_woodoku_board.py
@@ -1,11 +1,11 @@
+from typing import Tuple
+
 import numpy as np
 import pytest
 
 from exceptions.exceptions import ShapeOutOfBoardError
 from woodoku_board import WoodokuBoard
 from woodoku_shape import WoodokuShape
-
-N = 9
 
 
 class TestWoodokuBoard:
@@ -20,7 +20,7 @@ class TestWoodokuBoard:
         horizontal_bar_shape,
         gun_shape
     ])
-    def test_add_shape_on_empty_board(self, shape):
+    def test_add_shape_on_empty_board(self, shape: WoodokuShape):
         """
         Add shape to valid position on the board and remove all blocks of the shape to test add_shape
         """
@@ -34,7 +34,8 @@ class TestWoodokuBoard:
         (horizontal_bar_shape, (1, 0), gun_shape, (2, 0)),
         (gun_shape, (3, 0), horizontal_bar_shape, (1, 0))
     ])
-    def test_add_shape_on_board_without_conflict(self, first_shape, first_position, sec_shape, sec_position):
+    def test_add_shape_on_board_without_conflict(self, first_shape: WoodokuShape, first_position: Tuple[int, int],
+                                                 sec_shape: WoodokuShape, sec_position: Tuple[int, int]):
         """
         first_shape and first_location are chosen so that sec_shape will be NOT overlapped with first_shape when
         adding it at sec_location
@@ -50,9 +51,10 @@ class TestWoodokuBoard:
     @pytest.mark.parametrize('shape, location', [
         (l_shape, (0, 0)),
         (horizontal_bar_shape, (0, 3)),
-        (gun_shape, (N - 3, 0)),
+        (gun_shape, (6, 0)),
     ])
-    def test_can_add_shape_at_location_on_empty_board_within_board(self, shape, location):
+    def test_can_add_shape_at_location_on_empty_board_within_board(self, shape: WoodokuShape,
+                                                                   location: Tuple[int, int]):
         """
         Add shape to an empty board where each location to add is at the edge of the game board
         """
@@ -64,7 +66,8 @@ class TestWoodokuBoard:
         (horizontal_bar_shape, (4, 3)),
         (gun_shape, (4, 0)),
     ])
-    def test_can_add_shape_at_location_on_empty_board_go_beyond_board(self, shape, location):
+    def test_can_add_shape_at_location_on_empty_board_go_beyond_board(self, shape: WoodokuShape,
+                                                                      location: Tuple[int, int]):
         """
         Add shape to an empty board where location goes beyond the game board
         """
@@ -72,26 +75,27 @@ class TestWoodokuBoard:
         with pytest.raises(ShapeOutOfBoardError):
             board.can_add_shape_at_location(shape, *location)
 
-    @pytest.mark.parametrize('first_shape, first_location, sec_shape, sec_location', [
+    @pytest.mark.parametrize('first_shape, first_position, sec_shape, sec_position', [
         (l_shape, (0, 0), l_shape, (1, 0)),
         (horizontal_bar_shape, (4, 3), gun_shape, (4, 4)),
         (gun_shape, (4, 0), horizontal_bar_shape, (5, 0)),
     ])
-    def test_can_add_shape_at_occupied_location(self, first_shape, first_location, sec_shape, sec_location):
+    def test_can_add_shape_at_occupied_location(self, first_shape: WoodokuShape, first_position: Tuple[int, int],
+                                                sec_shape: WoodokuShape, sec_position: Tuple[int, int]):
         """
         first_shape and first_location are chosen so that sec_shape will be overlapped with first_shape when
         adding it at sec_location
         """
         board = WoodokuBoard()
-        board.add_shape(first_shape, *first_location)
-        assert not board.can_add_shape_at_location(sec_shape, *sec_location)
+        board.add_shape(first_shape, *first_position)
+        assert not board.can_add_shape_at_location(sec_shape, *sec_position)
 
     @pytest.mark.parametrize('shape', [
         l_shape,
         horizontal_bar_shape,
         gun_shape
     ])
-    def test_can_add_shape_to_board_on_empty_board(self, shape):
+    def test_can_add_shape_to_board_on_empty_board(self, shape: WoodokuShape):
         board = WoodokuBoard()
         assert board.can_add_shape_to_board(shape)
 
