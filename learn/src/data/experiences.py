1bdd1c28beba68e3a4f6f96f93d7027169e82282
Richard-Xiaorui Huang <hxr.richard@gmail.com>
2023-03-17 06:33:43 -0400

    design(experiences): design and implemented experience replay buffer
    implmentation is done so as to ensure that shape typing is functioning
    correctly

--- learn/src/data/experiences.py
+++ learn/src/data/experiences.py
@@ -1,33 +1,78 @@
-from typing import Tuple
+import numpy as np
+
+from jaxtyping import Float, Int, jaxtyped
 
 import torch
+from typeguard import typechecked
+from woodoku.env import observation_space_d, action_space_d
 
 
 class ExperienceReplay:
+    """
+    A class to store experiences replay buffer for WoodokuLearn reinforcement learning training.
+
+    Experiences are stored in a circular buffer, such that old experiences are automatically expired once the buffer is
+    full.
+
+    States need to be stored as float and passed through NN
+    Actions can be discrete here so we use int
+    Rewards although discrete in this case, but we will need to operate reward with float, so we will use float from the start
+
+    Attributes:
+        length: The length of the buffer.
+        index: The index to store the next experience.
+        is_full: Whether the buffer is full.
+        state: The state array.
+        action: The action array.
+        reward: The reward array.
+        next_state: The next state array.
+    """
+
     def __init__(self, length: int) -> None:
-        pass
-        # self.experience_replay = deque(maxlen=length)
+        self.length = length
+        self.index = 0
+        self.is_full = False
+        self.state = np.zeros((length, *observation_space_d))
+        self.action = np.zeros((length, *action_space_d), dtype=np.int_)
+        self.reward = np.zeros(length)
+        self.next_state = np.zeros((length, *observation_space_d))
 
     def collect(
-        self, experience: Tuple[torch.Tensor, int, float, torch.Tensor]
+        self,
+        experience: tuple[  # type: ignore[type-arg]
+            Float[np.ndarray, "*state"],
+            Int[np.ndarray, "*action"],
+            float,
+            Float[np.ndarray, "*state"],
+        ],
     ) -> None:
-        """
-        Args:
-            experience: list of state, action, reward, next_state
-        """
-        pass
-        # self.experience_replay.append(experience)
+        state, action, reward, next_state = experience
+        self.state[self.index] = state
+        self.action[self.index] = action
+        self.reward[self.index] = reward
+        self.next_state[self.index] = next_state
+        # index wraps around such that old experience auto expire
+        self.index = (self.index + 1) % self.length
+        if self.index == 0:
+            self.is_full = True
 
+    @jaxtyped  # type: ignore[misc]
+    @typechecked
     def sample_from_experience(
         self, sample_size: int
-    ) -> Tuple[torch.Tensor, int, float, torch.Tensor]:
-        pass
-        # if len(self.experience_replay) < sample_size:
-        #     sample_size = len(self.experience_replay)
-        # sample = random.sample(self.experience_replay, sample_size)
-
-        # state = torch.tensor([exp[0] for exp in sample]).float()
-        # action = torch.tensor([exp[1] for exp in sample]).float()
-        # reward = torch.tensor([exp[2] for exp in sample]).float()
-        # next_state = torch.tensor([exp[3] for exp in sample]).float()
-        # return state, action, reward, next_state
+    ) -> tuple[
+        Float[torch.Tensor, "batch *observation_space_d"],
+        Int[torch.Tensor, "batch *action_space_d"],
+        Float[torch.Tensor, "batch"],
+        Float[torch.Tensor, "batch *observation_space_d"],
+    ]:
+        if not self.is_full:
+            sample_size = min(self.index, sample_size)
+        sample_indices = np.random.choice(
+            self.index if not self.is_full else self.length, size=sample_size, replace=False
+        )
+        state = torch.tensor(self.state[sample_indices])
+        action = torch.tensor(self.action[sample_indices])
+        reward = torch.tensor(self.reward[sample_indices])
+        next_state = torch.tensor(self.next_state[sample_indices])
+        return state, action, reward, next_state
